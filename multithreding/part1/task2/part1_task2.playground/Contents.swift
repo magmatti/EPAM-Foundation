686e2cccf568021046966860c3fb0274e1e54f20
Mateusz Wójtowicz <matiwojt@gmail.com>
2025-07-29 18:36:16 +0200

    finished multithreading basics task implementation

---
+++ multithreding/part1/task2/part1_task2.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+// semaphore allowing 2 tasks at once
+let semaphore = DispatchSemaphore(value: 2)
+
+func performTask(id: Int) {
+    semaphore.wait()
+    print("Task \(id) started")
+    sleep(1) // Simulate a task taking time
+    print("Task \(id) finished")
+    semaphore.signal()
+}
+
+func runConcurrentTasks() {
+    let threads = (1...5).map { id in
+        Thread {
+            performTask(id: id)
+        }
+    }
+
+    threads.forEach { $0.start() }
+}
+
+runConcurrentTasks()
